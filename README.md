50512b3be628ed6cbd9dc1d7207ff06982ad7cbe
Jerrod Styrk <jstyrk@citadel.edu>
2020-06-08 16:13:01 -0700

    re factored and added a for each loop
    Co-authored-by: Sean Downs <spdowns@protonmail.com>
    Co-authored-by: Mook Dang <mookdang@gmail.com>

---
+++ README.md
@@ -0,0 +1,27 @@
+# Mad-Libs
+
+#### Mad-libs, loop and array practice Created on 06/08/2020
+
+#### By Jerrod Styrk, Mook Dang, Sean D.
+
+## We focus on refacting mad-lip page by include the arrey. 
+
+## Setup/Installation Requirements
+
+1. Go to the GitHub project repository
+2. Download file as a zip file to extract OR clone project using the .git URL.
+3. Navigate to folder and open index.html
+
+## Known Bugs
+No known bugs
+
+## Support and contact details
+For support Contact Jerrod Styrk, Mook Dang, and Sean D.
+
+## Technologies Used
+Used HTML, CSS and jQuery. Also bootstrap
+
+### License
+Copyright (c) 2016 Jerrod Styrk, Mook Dang, and Sean D.
+
+This software is licensed under MIT license.
--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,27 @@
 $(document).ready(function() {
-  $("#formOne").submit(function(event) {
-    const person1Input = $("input#person1").val();
-    const person2Input = $("input#person2").val();
-    const animalInput= $("input#animal").val();
-    const exclamationInput = $("input#exclamation").val();
-    const verbInput = $("input#verb").val();
-    const nounInput = $("input#noun").val();
+  console.log("Script executing!");
+  $("#formOne").submit(function() {
+    event.preventDefault();
+    // const person2Input = $("input#person2").val();
+    // const animalInput= $("input#animal").val();
+    // const exclamationInput = $("input#exclamation").val();
+    // const verbInput = $("input#verb").val();
+    // const nounInput = $("input#noun").val();
+    
+    let inputs = ["person1", "person2", "animal", "exclamation", "verb","noun"];
+    
+    inputs.forEach(function(input) {
+      let madInput = $("input#" + input).val(); 
+      $("." + input).text(madInput);
+      console.log(madInput)
+    });
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
+    // $(".person2").text(person2Input)
+    // $(".animal").text(animalInput);
+    // $(".exclamation").text(exclamationInput);
+    // $(".verb").text(verbInput);
+    // $(".noun").text(nounInput);
 
     $("#story").show();
-
-    event.preventDefault();
   });
 });
